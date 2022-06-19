f12f7e75c58c4495b2a7587e3f195c7f5022076d
TheGrandNobody <thegrandnobody@protonmail.com>
2022-06-19 20:39:38 +0200

    Add comments

--- generator.py
+++ generator.py
@@ -3,12 +3,13 @@ import secrets
 from eth_account import Account
 import sys
 
+# Create a new .csv file called "accounts.csv"
 with open('accounts.csv', 'w') as file:
     # Initialize the header
     writer = csv.writer(file)
     # Write the header row
     writer.writerow(['Address', 'Private Key'])
-
+    # Write a row containing a randomly generated address and private key for the specified number of times
     for i in range(int(sys.argv[1])):
         acc = Account.create(secrets.randbits(256))
         writer.writerow([acc.address, acc.privateKey.hex()[2:]])
