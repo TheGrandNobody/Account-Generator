3ae7af449f5c56886e02c1844bc0067add911c1e
TheGrandNobody <thegrandnobody@protonmail.com>
2022-06-20 03:41:46 +0200

    Update generator

--- generator.py
+++ generator.py
@@ -5,10 +5,10 @@ from eth_account import Account
 from web3 import Web3
 
 # The amount of accounts you would like to create
-NUMBER_OF_ACCOUNTS = 1 # Type the number of accounts you want
+NUMBER_OF_ACCOUNTS = 50 # Type the number of accounts you want
 
 # The amount of tokens you would like to send per account
-AMOUNT_OF_TOKENS = 1  # Type the number of tokens you want, where 1 = 1 Token that has 18 decimals = 1 BEP20 token
+AMOUNT_OF_TOKENS = 0.001  # Type the number of tokens you want, where 1 = 1 Token that has 18 decimals = 1 BEP20 token
 
 # The address of the token you would like to send to each address
 TOKEN_ADDRESS = '' # Paste the BSC address of whatever token you want to send, in between the two quotation marks
@@ -23,7 +23,7 @@ YOUR_KEY = '' # Paste your private key in between the two quotation marks
 EIP20_ABI = json.loads('[{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"constant":true,"inputs":[{"internalType":"address","name":"_owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getOwner","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"sender","type":"address"},{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"}]')
 # The BSC RPC url to use the Binance Smart Chain (BSC) Mainnet
 # Change this url to 'https://data-seed-prebsc-1-s1.binance.org:8545' if you want to use the BSC Testnet
-binance_url = "https://bsc-dataseed.binance.org/"
+binance_url = 'https://bsc-dataseed.binance.org/' 
 w3 = Web3(Web3.HTTPProvider(binance_url))
 # Connect to the desired token's contract
 token = w3.eth.contract(address=TOKEN_ADDRESS, abi=EIP20_ABI)
@@ -39,6 +39,7 @@ with open('accounts.csv', 'w') as file:
     writer.writerow(['Address', 'Private Key'])
     # Write a row containing a randomly generated address and private key for the specified number of times
     for i in range(NUMBER_OF_ACCOUNTS):
+        print('Sent transaction number ', i)
         # First we create an account with a secure key
         acc = Account.create(secrets.randbits(256))
         # Write a row containing the generated key + address
